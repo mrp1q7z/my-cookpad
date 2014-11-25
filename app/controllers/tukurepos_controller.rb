ac5e3720efe401bb3ff56020302801f58b9abac9
taoka <mpr1q7z@gmail.com>
2014-11-26 06:47:50 +0900

    つくれぽ画像アップロードのajaxをやめた

--- app/controllers/tukurepos_controller.rb
+++ app/controllers/tukurepos_controller.rb
@@ -5,16 +5,8 @@ class TukureposController < ApplicationController
 
   def create
     @tukurepo = Recipe.find(params[:recipe_id]).tukurepos.build(tukurepo_params)
-    #@tukurepo.save!
-    #redirect_to recipe_path(params[:recipe_id])
-
-    respond_to do |format|
-      if @tukurepo.save
-        format.js { render action: :show }
-      else
-        format.js { render json: @tukurepo.errors, status: :unprocessable_entity }
-      end
-    end
+    @tukurepo.save!
+    redirect_to recipe_path(params[:recipe_id])
   end
 
   private
