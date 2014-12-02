35335f6748211d2b73ee344c0c954acf11ba5361
taoka <mpr1q7z@gmail.com>
2014-12-02 09:45:39 +0900

    マイキッチンには下書き中のレシピも表示する

--- app/controllers/kitchen_controller.rb
+++ app/controllers/kitchen_controller.rb
@@ -2,7 +2,11 @@ class KitchenController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @recipes = user.recipes.published
+    if user == current_user
+      @recipes = user.recipes
+    else
+      @recipes = user.recipes.published
+    end
     @tukurepos = user.tukurepos
   end
 end
