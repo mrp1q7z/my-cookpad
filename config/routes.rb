7552c896771f8a9e1fd8efc7613d644f9841556b
taoka <mpr1q7z@gmail.com>
2014-12-05 05:43:49 +0900

    routesの順番を入れ替え

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'kitchen/:id', to: 'kitchen#show', as: 'kitchen'
-
   resources :email_magazines
+
   resources :recipes, only: [:new, :create, :edit, :show, :update] do
     member do
       delete :delete_image
       patch :publish
     end
-    get :search, on: :collection
     resources :directions, only: [:create, :update, :destroy] do
       member do
         delete :delete_image
@@ -16,11 +14,15 @@ Rails.application.routes.draw do
       end
     end
     resources :tukurepos, only: [:create]
+    get :search, on: :collection
   end
+
+  get 'kitchen/:id', to: 'kitchen#show', as: 'kitchen'
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
+
   root 'home#index'
 end
