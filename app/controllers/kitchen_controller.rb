3cf007d4a8611f9d64ef77ee21a24c9d21caa1b0
taoka <mpr1q7z@gmail.com>
2014-12-05 21:40:20 +0900

    ビューに渡す変数を減らした

--- app/controllers/kitchen_controller.rb
+++ app/controllers/kitchen_controller.rb
@@ -1,13 +1,5 @@
 class KitchenController < ApplicationController
   def show
-    user = User.find(params[:id])
-    # TODO: userだけを渡すとビュー側で取り出せる
-    @nickname = user.nickname
-    if user == current_user
-      @recipes = user.recipes
-    else
-      @recipes = user.recipes.published
-    end
-    @tukurepos = user.tukurepos
+    @user = User.find(params[:id])
   end
 end
