597b1b152980db9fcad184e458b2f2627ae21384
taoka <mpr1q7z@gmail.com>
2014-12-16 19:17:44 +0900

    direction_controllerをすっきり（DRYとビューに処理を移動）

--- app/controllers/directions_controller.rb
+++ app/controllers/directions_controller.rb
@@ -1,18 +1,16 @@
 class DirectionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_recipe
   before_action :set_direction, only: [:update, :destroy, :delete_image, :move_highter, :move_lower]
 
   def create
-    step = params[:step].to_i + 1
-    direction = Recipe.find(params[:recipe_id]).directions.build(step: step)
+    direction = @recipe.directions.build(direction_params)
     direction.save_and_reorder
-    @directions = Recipe.find(params[:recipe_id]).directions
     render :directions
   end
 
   def update
     @direction.update!(direction_params)
-    @directions = Recipe.find(params[:recipe_id]).directions
     respond_to do |format|
       format.html { redirect_to edit_recipe_path(@direction.recipe_id) }
       format.js { render :directions }
@@ -21,36 +19,36 @@ class DirectionsController < ApplicationController
 
   def destroy
     @direction.destroy!
-    @directions = Recipe.find(params[:recipe_id]).directions
     render :directions
   end
 
   def delete_image
     @direction.image = nil
     @direction.save
-    @directions = Recipe.find(params[:recipe_id]).directions
     render :directions
   end
 
   def move_highter
     @direction.move(:higher)
-    @directions = Recipe.find(params[:recipe_id]).directions
     render :directions
   end
 
   def move_lower
     @direction.move(:lower)
-    @directions = Recipe.find(params[:recipe_id]).directions
     render :directions
   end
 
   private
 
   def direction_params
-    params.require(:direction).permit(:image, :description)
+    params.require(:direction).permit(:image, :description, :step)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:recipe_id])
   end
 
   def set_direction
-    @direction = Recipe.find(params[:recipe_id]).directions.find(params[:id])
+    @direction = @recipe.directions.find(params[:id])
   end
 end
